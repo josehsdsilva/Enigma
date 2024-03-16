6d830364b6705d71d80c723c55ba3c3b77912f17
Francisco Andrade <f-infsec03@hotmail.com>
2024-03-16 15:59:14 +0000

    Player Movement and Clock Puzzle Working

---
+++ Enigma/Assets/Enigma/Scritps/Puzzles/ClockMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockMovement : MonoBehaviour
+{
+    GameObject currClockPointer;
+    [SerializeField] Camera cam;
+    [SerializeField] GameObject bigClockPointer;
+    [SerializeField] GameObject smallClockPointer;
+    [SerializeField] Button moveClockPointerRightBtn, moveClockPointerLeftBtn;
+    PuzzleResolution puzzleResolution;
+
+    private void Start()
+    {
+        currClockPointer = bigClockPointer;
+        puzzleResolution = GetComponent<PuzzleResolution>();
+
+        moveClockPointerLeftBtn.onClick.AddListener(() => MoveClockPointers(30));
+        moveClockPointerRightBtn.onClick.AddListener(() => MoveClockPointers(-30));
+    }
+
+    private void Update()
+    {
+        GetCollision();
+    }
+
+    private void GetCollision()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Ray ray = cam.ScreenPointToRay(mousePosition);
+
+            RaycastHit2D raycast2D = Physics2D.GetRayIntersection(ray);
+
+            if (raycast2D.collider && raycast2D.collider.gameObject.CompareTag("BigClockPointer"))
+            {
+                currClockPointer = bigClockPointer;
+            }
+            else if (raycast2D.collider && raycast2D.collider.gameObject.CompareTag("SmallClockPointer")) 
+            {
+                currClockPointer = smallClockPointer;
+            } 
+        }
+    }
+
+    private void MoveClockPointers(int _degrees)
+    {
+        currClockPointer.transform.Rotate(new Vector3(0, 0, _degrees), Space.World);
+        float smallPointerAngle = smallClockPointer.transform.rotation.eulerAngles.z;
+        float bigPointerAngle = bigClockPointer.transform.rotation.eulerAngles.z;
+        puzzleResolution.GetAnglesForPointers(smallPointerAngle, bigPointerAngle);
+    }
+}
